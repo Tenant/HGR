315c30e1e7c64292f7819d337d0005d3cb2c16d9
Gao Shuqi <gaoshq@outlook.com>
2018-04-08 14:18:43 +0800

    Add main function

--- imDiff.m
+++ imDiff.m
@@ -5,7 +5,7 @@ Reference: R. T. Collins et al.
 By Gao Shuqi, 0n 2018-03-30
 Test Data: imDiff_test_Data_mat
 %}
-function im_out=imDiff(im_in_1, im_in_2, im_in_3)
+function im_out=imDiff(im_in_1, im_in_2, im_in_3, threshold)
 
     if ndims(im_in_1)==3,% RGB is given
          im_in_1 = rgb2gray(im_in_1);
@@ -19,7 +19,6 @@ function im_out=imDiff(im_in_1, im_in_2, im_in_3)
          im_in_3 = rgb2gray(im_in_3);
     end 
 
-    threshold=8; % defalut value= 8
     mask=(im_in_3-im_in_2)>threshold & (im_in_3-im_in_1)>threshold;
     im_out=im_in_3;
     im_out(~mask)=0;
---
+++ main.m
@@ -0,0 +1,36 @@
+%% Workspace Initialization
+clc; clf; close all; clear all;
+FramesPerTrigger=20;
+%% Device Configuration
+vid = videoinput('winvideo', 1, 'YUY2_320x240');
+vid.ReturnedColorspace = 'rgb';
+vid.FramesPerTrigger = FramesPerTrigger;
+vid.TriggerFrameDelay = 5;
+src.FrameRate ='30';
+start(vid);
+[frames, timeStamp]=getdata(vid);
+
+%% Background Image Difference
+%%{
+threshold=8; % defalut value= 8
+for ii=3:FramesPerTrigger
+    ROI=imDiff(frames(:, :, :, ii-2), frames(:, :, :, ii-1), frames(:, :, :, ii), threshold);
+    ROI=im2bw(ROI, graythresh(ROI));
+    figure(); imshow(ROI);
+end
+%}
+%% Background Substraction
+%{
+bgd=frames(:, :, :, 1);
+width=size(bgd,1);
+height=size(bgd, 2);
+Threshold=zeros(width, height)+10; % recommended value: 10
+learning_rate=0.6; % recommended value: 0.7
+for ii=1:FramesPerTrigger
+    im_in=frames(:, :, :, ii);
+    [im_out, bgd, Threshold]=bgdSub(im_in, bgd, learning_rate, Threshold);
+    im_out=im2bw(im_out, graythresh(im_out));
+    figure(); imshow(im_out);
+end
+%}
+%% 
