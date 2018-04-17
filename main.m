93e88681727ba7c25c93a88a4efcb355ec4d1af2
Gao Shuqi <gaoshq@outlook.com>
2018-04-17 16:33:04 +0800

    主程序

--- main.m
+++ main.m
@@ -11,46 +11,67 @@ src.FrameRate ='30';
 start(vid);
 [frames, timeStamp]=getdata(vid);
 
-%% Background Image Difference
-%{
+%% Region Extraction
+% Background Image Difference
 threshold=8; % defalut value= 8
 
-for ii=3:FramesPerTrigger
-    ROI=imDiff(frames(:, :, :, ii-2), frames(:, :, :, ii-1), frames(:, :, :, ii), threshold);
-    ROI=im2bw(ROI, graythresh(ROI));
-    figure(); imshow(ROI);
-end
-%}
-
-%% Background Substraction
-%%{
+% Background Substraction
 bgd=frames(:, :, :, 1);
 width=size(bgd,1);
 height=size(bgd, 2);
 Threshold=zeros(width, height)+10; % recommended value: 10
 learning_rate=0.7; % recommended value: 0.7
 
-for ii=2:FramesPerTrigger
-    im_in=frames(:, :, :, ii);
-    [im_out, bgd, Threshold]=bgdSub(im_in, bgd, learning_rate, Threshold);
-    im_out=im2bw(im_out, graythresh(im_out));
-    figure(); imshow(im_out);
-end
-%}
-%% Skin-color Probability Map - Offline Training
-% SPM_train()
-% SPM_test_batch()
+% skin-color Probability Map - Offline Training
+load SPM_off
 
-%% Skin-color Probability Map - Online Training
+% Skin-color Probability Map - Online Training
 skinPixels = 0;
 nonskinPixels = 0;
 numTable(2, 1:32, 1:32, 1:32) = 0;
+% im_ROI=
+% im_bgd=
 
-for ii=1:FramesPerTrigger
-    [SPM_on, numTable, skinPixels, nonskinPixels]=SPM_train_online(im_ROI, im_bgd, numTable, skinPixels, nonskinPixels);
+% skin-color classifier
+threshold_SPM=0.8;
+SPM=SPM_off;
+
+for ii=3:FramesPerTrigger
+    % Background Image Difference
+    ROI_1=imDiff(frames(:, :, :, ii-2), frames(:, :, :, ii-1), frames(:, :, :, ii), threshold);
+    ROI_1=im2bw(ROI_1, graythresh(ROI_1));
+    
+    % Background Substraction
+    im_in=frames(:, :, :, ii);
+    [im_out, bgd, Threshold]=bgdSub(im_in, bgd, learning_rate, Threshold);
+    ROI_2=im2bw(im_out, graythresh(im_out));
+    
+    % skin-color Probability Map - Online Training
+    % [SPM_on, numTable, skinPixels, nonskinPixels]=SPM_train_online(im_ROI, im_bgd, numTable, skinPixels, nonskinPixels);
+    
+    % Skin color classifier
+    % lr=ii/FramesPerTrigger;
+    % SPM=(1-lr)*SPM_off+lr*SPM_on;
+    ROI_3=SPM_test_main(frames(:, :, :, ii), SPM, threshold_SPM);
+    % P_skin=(1-lr)*P_skin_off+lr*P_skin_on;
+    % P_nonskin=(1-lr)*P_nonskin_off+lr*P_nonskin_on;
+    
+    % Combination of Information
+    
+    
+    % prepare for next round
+    % im_ROI=
+    % im_bgd=
+    
+    % figure
+    %{
+    figure;
+    subplot(1,3,1); imshow(ROI_1);
+    subplot(1,3,2); imshow(ROI_2);
+    subplot(1,3,3); imshow(ROI_3);
+    %}
+    
+    
     
 end
 
-%% Skin color classifier
-
-%% Calculation of Morphologyy Weights
