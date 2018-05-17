409fefc5fa47093ad6c7134e74b0d038c28bdc46
Gao Shuqi <gaoshq@outlook.com>
2018-05-17 12:10:43 +0800

    Complete mark(); mark-wise label for image

--- mark.m
+++ mark.m
@@ -5,12 +5,20 @@ function im_out=mark(im_in, mask)
     Height=size(im_in,1);
     Width=size(im_in,2);
     
+    % 如果图像为灰度图，则将其转换为伪彩色图像
+     if(size(im_in,3)==1)
+        im_out(:,:,1)=im_in;
+        im_out(:,:,2)=im_in;
+        im_out(:,:,3)=im_in;
+        im_in=im_out;
+    end
+    
     im_out=im_in;
     
     for ii=1:Height
         for jj=1:Width
             if(mask(ii,jj))
-                im_out(ii,jj,:)=[0,0,255];
+                im_out(ii,jj,:)=[50 205 50];
             end
         end
     end
