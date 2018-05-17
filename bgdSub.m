8eddad6b20d97072551d92af80318a69b98e0c53
Gao Shuqi <gaoshq@outlook.com>
2018-05-17 10:54:40 +0800

    Complete bgdSub(); mROI segmetation based on Background Substraction Algorithm.

--- bgdSub.m
+++ bgdSub.m
@@ -1,41 +1,44 @@
-%{
-Motion Detection Script
-Backgroud Substraction
-Reference: R. T. Collins et al.
-By Gao Shuqi, 0n 2018-03-30
-Test Data: bgdSub_test_data
-%}
-function [im_out, bgd, T]=bgdSub(im_in, bgd0, a, T0)
-    [m, n, k]=size(im_in);
+function [mask, bgd, threshold]=bgdSub(img, bgd, learningrate, threshold)
+
+   [m, n, k]=size(img);
    Euler=zeros(m, n);
-    for ii=1:m
-        for jj=1:n
-            tmp=(im_in(ii, jj, 1)-bgd0(ii, jj, 1))^2+(im_in(ii, jj, 2)-bgd0(ii, jj, 2))^2+(im_in(ii, jj, 3)-bgd0(ii, jj, 3))^2;
-            Euler(ii, jj)=uint8(sqrt(double(tmp)));
+   
+   if k==3
+        for ii=1:m
+            for jj=1:n
+                tmp=(img(ii, jj, 1)-bgd(ii, jj, 1))^2+(img(ii, jj, 2)-bgd(ii, jj, 2))^2+(img(ii, jj, 3)-bgd(ii, jj, 3))^2;
+                Euler(ii, jj)=uint8(sqrt(double(tmp)));
+            end
         end
+   else
+       for ii=1:m
+           for jj=1:n
+               tmp=max(img(ii,jj)-bgd(ii,jj),0);
+               Euler(ii,jj)=uint8(sqrt(double(tmp)));
+           end
+       end
+   end
+    mask=Euler>threshold;
+ 
+    if k==1
+        bgd(~mask)=learningrate*bgd(~mask)+(1-learningrate)*img(~mask);
+    else
+        R=bgd(:,:,1);
+        G=bgd(:,:,2);
+        B=bgd(:,:,3);
+        R_=img(:,:,1);
+        G_=img(:,:,2);
+        B_=img(:,:,3);
+        R(~mask)=learningrate*R(~mask)+(1-learningrate)*R_(~mask);
+        G(~mask)=learningrate*G(~mask)+(1-learningrate)*G_(~mask);
+        B(~mask)=learningrate*B(~mask)+(1-learningrate)*B_(~mask);
+        bgd(:,:,1)=R;
+        bgd(:,:,2)=G;
+        bgd(:,:,3)=B;
     end
-    mask=Euler>T0;
-    im_out=rgb2gray(im_in);
-    im_out(~mask)=0;
     
-    % bgd=a*bgd0+(1-a)*im_in;
-    bgd=bgd0;
-    R=bgd(:,:,1);
-    G=bgd(:,:,2);
-    B=bgd(:,:,3);
-    R(~mask)=a*R(~mask)+(1-a)*im_in(~mask);
-    G(~mask)=a*G(~mask)+(1-a)*im_in(~mask);
-    B(~mask)=a*B(~mask)+(1-a)*im_in(~mask);
-    bgd(:,:,1)=R;
-    bgd(:,:,2)=G;
-    bgd(:,:,3)=B;
-    
-    T=T0;
-%     maxium=max(Euler(:));
-%     BND=zeros(size(Euler))+maxium; % maxium
-%     TMP=BND-Euler;
-    T(~mask)=a*T0(~mask)+5*(1-a)*Euler(~mask);
-    tmp=T>10;
-    T(~tmp)=10;
+    threshold(~mask)=learningrate*threshold(~mask)+5*(1-learningrate)*Euler(~mask);
+    TMP=threshold>6; % ### 背景去除算法阈值下限 precious=3
+    threshold(~TMP)=6;
 
 end
