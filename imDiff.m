dc9735092cff54afbb3220b022c7886a4c0ad60c
Gao Shuqi <gaoshq@outlook.com>
2018-05-17 12:09:50 +0800

    Complete imDiff(); mROI segmetation based on Image Difference Algorithm

--- imDiff.m
+++ imDiff.m
@@ -5,23 +5,21 @@ Reference: R. T. Collins et al.
 By Gao Shuqi, 0n 2018-03-30
 Test Data: imDiff_test_Data_mat
 %}
-function im_out=imDiff(im_in_1, im_in_2, im_in_3, threshold)
+function mask=imDiff(im_in_1, im_in_2, im_in_3, threshold)
 
-    if ndims(im_in_1)==3,% RGB is given
+    if ndims(im_in_1)==3% RGB is given
          im_in_1 = rgb2gray(im_in_1);
     end 
     
-    if ndims(im_in_2)==3,% RGB is given
+    if ndims(im_in_2)==3% RGB is given
          im_in_2 = rgb2gray(im_in_2);
     end 
     
-    if ndims(im_in_3)==3,% RGB is given
+    if ndims(im_in_3)==3% RGB is given
          im_in_3 = rgb2gray(im_in_3);
     end 
 
     mask=(im_in_3-im_in_2)>threshold & (im_in_3-im_in_1)>threshold;
-    im_out=im_in_3;
-    im_out(~mask)=0;
     
 end
 
